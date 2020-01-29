ddac2619bd598d3b20f24377c3a7e46fd26f3378
iuliancioarca <iulian.cioarca@gmail.com>
2020-01-29 19:30:54 +0200

    Add files via upload

--- INSTR_TYPES.jl
+++ INSTR_TYPES.jl
@@ -1,18 +1,18 @@
 # Instrument types
 # PSU
 E364x1 = Union{INSTR{:KeysightE3645A},
-					INSTR{:AgilentE3645A}
-					}
+	INSTR{:AgilentE3645A}
+	}
 E364x2 = Union{INSTR{:KeysightE3646A},
-					INSTR{:AgilentE3646A},
-					INSTR{:KeysightE3649A},
-					INSTR{:AgilentE3649A}
-					}
+	INSTR{:AgilentE3646A},
+	INSTR{:KeysightE3649A},
+	INSTR{:AgilentE3649A}
+	}
 					
 # FGEN
 F332x1 = Union{INSTR{:Agilent33250A}
-					}
+	}
 F335x2 = Union{INSTR{:Agilent33500B},
-					INSTR{:Keysight33500B},
-					INSTR{:Agilent33522A}
-					}
+	INSTR{:Keysight33500B},
+	INSTR{:Agilent33522A}
+	}
