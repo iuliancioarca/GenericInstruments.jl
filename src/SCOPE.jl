545cfab835b34d7d5418c6a83038d60b2cb3452a
iuliancioarca <iulian.cioarca@gmail.com>
2020-03-19 20:16:24 +0200

    Delete SCOPE.jl

--- src/SCOPE.jl
+++
@@ -1,8 +0,0 @@
-module SCOPE
-include("comm_utils.jl")
-include("INSTR_TYPES.jl")
-include("scope_common.jl")
-# specifics by manufacturer/model
-include("LecroyHDO6054A.jl")
-include("niScope.jl")
-end #endmodule
