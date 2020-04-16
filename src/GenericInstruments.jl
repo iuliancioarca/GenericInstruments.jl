f9d375365b9c376ed723e35ffd062a77a12cd408
iuliancioarca <iulian.cioarca@gmail.com>
2020-04-16 12:23:30 +0300

    Update GenericInstruments.jl

--- src/GenericInstruments.jl
+++ src/GenericInstruments.jl
@@ -11,10 +11,10 @@ end
 include("INSTR_TYPES.jl")
 include("comm_utils.jl")
 # instruments
-include("psu\\PSU.jl")
-include("awg\\AWG.jl")
-include("scope\\SCOPE.jl")
-include("dmm\\DMM.jl")
+include("psu/PSU.jl")
+include("awg/AWG.jl")
+include("scope/SCOPE.jl")
+include("dmm/DMM.jl")
 
 using .PSU
 using .AWG
