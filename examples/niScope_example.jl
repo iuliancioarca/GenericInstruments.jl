92744b08624188ba34dad3ac9ff27906de8f9f78
iuliancioarca <iulian.cioarca@gmail.com>
2020-04-16 19:15:47 +0300

    Tested with new package.Triggering has some issues

--- examples/niScope_example.jl
+++ examples/niScope_example.jl
@@ -12,24 +12,24 @@ GI.SCOPE.niScope_AutoSetup(niScope1)
 
 ### User defined low_level
 # Channel coupling
-GI.SCOPE.set_coupling(niScope1;ch="1",cpl=2)
-GI.SCOPE.niScope_GetAttributeViInt32(niScope1, "1", 1250003) #OK
+GI.SCOPE.set_coupling(niScope1;ch="1",cpl="gnd")
+GI.SCOPE.niScope_GetAttributeViInt32(niScope1, "0", 1250003) # OK
 # Channel state on/off
-GI.SCOPE.set_ch_state(niScope1;ch="1",st="on")
-GI.SCOPE.niScope_GetAttributeViBoolean(niScope1, "1", 1250005) #OK
+GI.SCOPE.set_state(niScope1;ch="1",st="on")
+GI.SCOPE.niScope_GetAttributeViBoolean(niScope1, "0", 1250005) #OK
 # Probe attenuation
-GI.SCOPE.set_atten(niScope1;ch="1",atten=100.0)
-GI.SCOPE.niScope_GetAttributeViReal64(niScope1, "1", 1250004) # always returns 1.0!!!
+GI.SCOPE.set_atten(niScope1;ch="1",att="10x")
+GI.SCOPE.niScope_GetAttributeViReal64(niScope1, "0", 1250004) # always returns 1.0!!!
 # BW limit
-GI.SCOPE.set_bw_lim(niScope1;ch="1",bw=20000000.0)
-GI.SCOPE.niScope_GetAttributeViReal64(niScope1, "1", 1250006) #OK
+GI.SCOPE.set_bw_lim(niScope1;ch="1",bw="100MHz")
+GI.SCOPE.niScope_GetAttributeViReal64(niScope1, "0", 1250006) #OK
 ## VERTICAL
 # Vrange
-GI.SCOPE.set_vrange(niScope1;ch="1",vrang=0.35)
-GI.SCOPE.niScope_GetAttributeViReal64(niScope1, "1", 1250001) #OK, it approximates to next value
+GI.SCOPE.set_vrange(niScope1;ch="1",vrang=2.2)
+GI.SCOPE.niScope_GetAttributeViReal64(niScope1, "0", 1250001) #OK, it approximates to next value
 # Vertical offset
 GI.SCOPE.set_voffs(niScope1;ch="1",voffs=0.12)
-GI.SCOPE.niScope_GetAttributeViReal64(niScope1, "1", 1250002) #OK
+GI.SCOPE.niScope_GetAttributeViReal64(niScope1, "0", 1250002) #OK
 ## HORIZONTAL
 # Nr of acquisition points 
 GI.SCOPE.set_nr_pts(niScope1;npts=1.3e6)
@@ -38,15 +38,15 @@ GI.SCOPE.niScope_GetAttributeViInt32(niScope1, "", 1250009) #OK
 GI.SCOPE.set_srate(niScope1;sr=1.6e6) # take care, this is min sample rate, not actual sample rate
 GI.SCOPE.niScope_GetAttributeViReal64(niScope1, "", 1150009) #OK, min sample rate
 GI.SCOPE.niScope_GetAttributeViReal64(niScope1, "", 1250010) #OK, actual sample rate:1.6129032258064516e6
-# Horizontal offset origin
+# Horizontal offset origin in #divisions
 GI.SCOPE.set_hoffs_div(niScope1;href=1) # actually sends 1*10 = 10%
 GI.SCOPE.niScope_GetAttributeViReal64(niScope1, "", 1250011) #OK, returns 1*10 = 10%
 ## TRIGGER
 # Set trigger type
-GI.SCOPE.set_trig_typ(niScope1;tp=3)
+GI.SCOPE.set_trig_type(niScope1;tp="edge")
 GI.SCOPE.niScope_GetAttributeViInt32(niScope1, "", 1250012) #OK, find triggertype enum!
 # Set trigger source
-GI.SCOPE.set_trig_src(niScope1;val="VAL_EXTERNAL") #can be channel:"0" or "VAL_EXTERNAL", etc. check enum
+GI.SCOPE.set_trig_src(niScope1;src="1") #can be channel:"0" or "ext", etc. check enum
 GI.SCOPE.niScope_GetAttributeViString(niScope1, "", 1250013) #OK
 # Set trigger slope
 GI.SCOPE.set_trig_slope(niScope1;slp=1)
@@ -61,8 +61,8 @@ GI.SCOPE.niScope_GetAttributeViReal64(niScope1, "", 1250016) #OK
 GI.SCOPE.set_hoffs_t(niScope1;href=1.2e-3)
 GI.SCOPE.niScope_GetAttributeViReal64(niScope1, "", 1250015) #OK
 # Set trigger coupling
-GI.SCOPE.set_trig_cpl(niScope1;ch="1",cpl=1001)
-GI.SCOPE.niScope_GetAttributeViInt32(niScope1, "1", 1250014) #always returns 0
+GI.SCOPE.set_trig_cpl(niScope1;ch="1",cpl="ac")
+GI.SCOPE.niScope_GetAttributeViInt32(niScope1, "", 1250014) #always returns 0
 
 # Built-in high level functions
 GI.SCOPE.niScope_ConfigureVertical(niScope1,
@@ -72,5 +72,8 @@ minSampleRate=2e5,minNumPts=1000,refPosition=0.1,numRecords=1,enforceRealtime=1)
 GI.SCOPE.niScope_ConfigureTriggerImmediate(niScope1)
 GI.SCOPE.niScope_InitiateAcquisition(niScope1)
 GI.SCOPE.niScope_ActualRecordLength(niScope1)
-y,info = GI.SCOPE.fetch_wfm(niScope1,ch=0)
+y,info = GI.SCOPE.fetch_wfm(niScope1,ch="1")
 plot(y)
+
+# Close
+GI.SCOPE.niScope_close(niScope1)
