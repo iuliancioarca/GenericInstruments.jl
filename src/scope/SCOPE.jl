1aff33d64145f13611b1002dc03e5f1fb91fd297
iuliancioarca <iulian.cioarca@gmail.com>
2020-04-16 11:33:31 +0300

    Update SCOPE.jl

--- src/scope/SCOPE.jl
+++ src/scope/SCOPE.jl
@@ -6,10 +6,27 @@ include("..\\INSTR_TYPES.jl")
 include("..\\comm_utils.jl")
 #
 include("SCOPE_common.jl")
+# Lecroy
 # specifics by manufacturer/model
-include("LecroyHDO6054A.jl")
-include("NIScope_low_level.jl")
-include("NIScope_high_level_built_in.jl")
-include("NIScope_low_level_user.jl")
-include("NIScope.jl")
+include(".\\LecroyHDO6054A\\LCR6xxx_Acquisition.jl")
+include(".\\LecroyHDO6054A\\LCR6xxx_Horizontal.jl")
+include(".\\LecroyHDO6054A\\LCR6xxx_Math.jl")
+include(".\\LecroyHDO6054A\\LCR6xxx_Measurements.jl")
+include(".\\LecroyHDO6054A\\LCR6xxx_Misc.jl")
+include(".\\LecroyHDO6054A\\LCR6xxx_Synchronization.jl")
+include(".\\LecroyHDO6054A\\LCR6xxx_Triggering.jl")
+include(".\\LecroyHDO6054A\\LCR6xxx_Vertical.jl")
+
+# PXI
+include(".\\NIScope\\NIScope.jl")
+include(".\\NIScope\\NIScope_Acquisition.jl")
+include(".\\NIScope\\NIScope_high_level_built_in.jl")
+include(".\\NIScope\\NIScope_Horizontal.jl")
+include(".\\NIScope\\NIScope_low_level.jl")
+include(".\\NIScope\\NIScope_Math.jl")
+include(".\\NIScope\\NIScope_Measurements.jl")
+include(".\\NIScope\\NIScope_Misc.jl")
+include(".\\NIScope\\NIScope_Synchronization.jl")
+include(".\\NIScope\\NIScope_Triggering.jl")
+include(".\\NIScope\\NIScope_Vertical.jl")
 end #endmodule
